635c528309ede96e5718dbdcbd53cc3a015ee074
[Milos Dimitrijevic] <somidimikv@gmail.com>
2019-10-28 23:54:58 +0100

    I pushed homework from monday, week 2

---
+++ Nedelja2/cetvrtak-forme i tabele/zadatak2.js
@@ -0,0 +1,5 @@
+var variable = 'Welcome to DOM';
+document.getElementById("mainheading").innerHTML= variable;
+document.getElementById("newblock").innerHTML = "<p>Neki proizvoljni tekst</p>"
+
+console.log((0/0) == "NaN");
---
+++ Nedelja2/zadatak1.js
@@ -0,0 +1,9 @@
+var numberFirst = 5;
+var numberSecond = 2;
+
+console.log(numberFirst+numberSecond);
+console.log(numberFirst-numberSecond);
+console.log(numberFirst*numberSecond);
+console.log(numberFirst/numberSecond);
+console.log(numberFirst%numberSecond);
+console.log(numberFirst**numberSecond);
